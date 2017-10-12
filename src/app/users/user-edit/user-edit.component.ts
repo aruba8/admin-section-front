ef86eae2294d1f234211384d36eca20fd304ad2b
Erik Khalimov <biomaks@gmail.com>
2017-10-12 09:06:37 -0500

    edit user page

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {UsersService} from '../users.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {UserModel} from '../user.model';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  userId: number;
+  editedUser: UserModel;
 
-  constructor() { }
+  constructor(private usersService: UsersService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.userId = +params['id'];
+        this.editedUser = this.usersService.getUsers()[this.userId];
+      }
+    );
+  }
+
+  onCancel() {
+    this.router.navigate(['/users']);
+
   }
 
 }
